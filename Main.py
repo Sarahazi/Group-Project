d7ed3a2419b9965352716bfa73f5dc019397fd4b
EwmJ4y <133128958+EwmJ4y@users.noreply.github.com>
2023-06-22 16:17:39 +0200

    Main.py

--- Main.py
+++ Main.py
@@ -25,15 +25,35 @@ def savings_goals_menu():
     # Add your savings goals-related options here
     csvreader.getSavGoal()
                 
+import csvreader
+import csv
+
+def show_menu():
+    print("\n===== Budget Management System =====")
+    print("1. Expenses")
+    print("2. Savings Goals")
+    print("3. Budgets")
+    print("4. Exit")
+
+def expenses_menu():
+    print("\n===== Expenses Menu =====")
+    # Add your expenses-related options here
+    csvreader.getExpCat()
+
+def savings_goals_menu():
+    print("\n===== Savings Goals Menu =====")
+    # Add your savings goals-related options here
+    csvreader.getSavGoal()
+                
 def budgets_menu():
     print("\n===== Budgets Menu =====")
     csv_income = 'income.csv'
     with open(csv_income) as f:
-        total_income = sum(int(r['Value']) for r in csv.DictReader(f))
+        total_income = sum(int(r['Amount(Euro)']) for r in csv.DictReader(f))
             
     csv_expenses = 'expenses.csv'
     with open(csv_expenses) as f:
-        total_expenses = sum(int(r['Value']) for r in csv.DictReader(f))
+        total_expenses = sum(int(r['Amount(Euro)']) for r in csv.DictReader(f))
 
     if total_income is not None and total_expenses is not None:
         total_balance = total_income - total_expenses
@@ -53,7 +73,6 @@ def create_custom_budget(total_balance):
         print(f"Remaining balance: {remaining_balance}")
     except ValueError:
         print("Invalid input. Please enter a valid number for the allocated amount.")
-
 
 def main():
     while True:
